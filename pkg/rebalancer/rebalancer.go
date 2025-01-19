df02bba49fa9c16794e2ed3484428283a4c8d421
Nishchal Gautam <gautam.nishchal@gmail.com>
2025-01-19 10:48:58 +0700

    refactor: parameterize and add ability to get the load balancer

--- pkg/rebalancer/rebalancer.go
+++ pkg/rebalancer/rebalancer.go
@@ -13,38 +13,47 @@ type LoadBalancer interface {
 	ResetWithNewItems([]*httputil.ReverseProxy)
 }
 
-type Rebalancer struct {
+type Rebalancer[T LoadBalancer] struct {
 	logger  *logrus.Logger
 	proxies []*httputil.ReverseProxy
-	lb      LoadBalancer
+	lb      T
 }
 
-func (r *Rebalancer) StartRebalancer() {
+func (r *Rebalancer[T]) StartRebalancer(interval time.Duration, livenessEndpoint string) {
+	ticker := time.NewTicker(interval)
 	for {
-		time.Sleep(time.Second)
-		healthy, unhealthy := lo.FilterReject(r.proxies, func(p *httputil.ReverseProxy, _ int) bool {
-			request, err := http.NewRequest("GET", "/live", nil)
-			if err != nil {
-				return false
-			}
-			resp, err := p.Transport.RoundTrip(request)
-			if err != nil {
-				return false
-			}
-
-			return resp.StatusCode == http.StatusOK
-		})
+		healthy, unhealthy := r.groupProxiesByLiveness(livenessEndpoint)
 		if len(unhealthy) != 0 {
 			r.logger.WithField("count", len(unhealthy)).Warn("Found some unhealthy processes")
 		}
 		r.lb.ResetWithNewItems(healthy)
+		<-ticker.C
 	}
 }
 
-func New(proxies []*httputil.ReverseProxy, lb LoadBalancer, logger *logrus.Logger) *Rebalancer {
+func (r *Rebalancer[T]) GetLoadbalancer() T {
+	return r.lb
+}
+
+func (r *Rebalancer[T]) groupProxiesByLiveness(livenessEndpoint string) ([]*httputil.ReverseProxy, []*httputil.ReverseProxy) {
+	return lo.FilterReject(r.proxies, func(p *httputil.ReverseProxy, _ int) bool {
+		request, err := http.NewRequest("GET", livenessEndpoint, nil)
+		if err != nil {
+			return false
+		}
+		resp, err := p.Transport.RoundTrip(request)
+		if err != nil {
+			return false
+		}
+
+		return resp.StatusCode == http.StatusOK
+	})
+}
+
+func New[T LoadBalancer](proxies []*httputil.ReverseProxy, lb T, logger *logrus.Logger) *Rebalancer[T] {
 	lb.ResetWithNewItems(proxies)
 
-	return &Rebalancer{
+	return &Rebalancer[T]{
 		lb:      lb,
 		logger:  logger,
 		proxies: proxies,
--- pkg/rebalancer/rebalancer_test.go
+++ pkg/rebalancer/rebalancer_test.go
@@ -2,12 +2,14 @@
 package rebalancer_test
 
 import (
-	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http/httputil"
 	"net/url"
 	"testing"
 	"time"
+
+	"github.com/cyberhck/roundguard/pkg/roundrobin"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/cyberhck/roundguard/mocks"
 	"github.com/cyberhck/roundguard/pkg/rebalancer"
@@ -22,23 +24,62 @@ func TestNew(t *testing.T) {
 		count := 0
 		mockLb.EXPECT().ResetWithNewItems(gomock.Any()).DoAndReturn(func(proxies []*httputil.ReverseProxy) {
 			if count == 0 {
-				assert.Len(t, proxies, 2)
+				assert.Len(t, proxies, 3)
+			}
+			if count == 1 {
+				assert.Len(t, proxies, 1)
+			}
+			count++
+		}).AnyTimes()
+		tr1 := httpmock.NewMockTransport()
+		tr1.RegisterNoResponder(httpmock.NewStringResponder(500, ""))
+		p1 := httputil.NewSingleHostReverseProxy(&url.URL{})
+		p1.Transport = tr1
+		tr2 := httpmock.NewMockTransport()
+		tr2.RegisterNoResponder(httpmock.NewStringResponder(200, ""))
+		p2 := httputil.NewSingleHostReverseProxy(&url.URL{})
+		p2.Transport = tr2
+		tr3 := httpmock.NewMockTransport()
+		p3 := httputil.NewSingleHostReverseProxy(&url.URL{})
+		p3.Transport = tr3
+		logger := logrus.New()
+		logger.SetOutput(io.Discard)
+		rb := rebalancer.New([]*httputil.ReverseProxy{p1, p2, p3}, mockLb, logger)
+		go rb.StartRebalancer(time.Millisecond*200, "/live")
+		time.Sleep(time.Microsecond * 250)
+	})
+	t.Run("if liveness endpoint is invalid, it removes everything", func(t *testing.T) {
+		mockLb := mocks.NewMockLoadBalancer(gomock.NewController(t))
+		count := 0
+		mockLb.EXPECT().ResetWithNewItems(gomock.Any()).DoAndReturn(func(proxies []*httputil.ReverseProxy) {
+			if count == 0 {
+				assert.Len(t, proxies, 3)
 			}
 			if count == 1 {
 				assert.Len(t, proxies, 0)
 			}
 			count++
 		}).AnyTimes()
-		tr := httpmock.NewMockTransport()
-		tr.RegisterNoResponder(httpmock.NewStringResponder(500, ""))
+		tr1 := httpmock.NewMockTransport()
+		tr1.RegisterNoResponder(httpmock.NewStringResponder(500, ""))
 		p1 := httputil.NewSingleHostReverseProxy(&url.URL{})
-		p1.Transport = tr
+		p1.Transport = tr1
+		tr2 := httpmock.NewMockTransport()
+		tr2.RegisterNoResponder(httpmock.NewStringResponder(200, ""))
 		p2 := httputil.NewSingleHostReverseProxy(&url.URL{})
-		p2.Transport = tr
+		p2.Transport = tr2
+		tr3 := httpmock.NewMockTransport()
+		p3 := httputil.NewSingleHostReverseProxy(&url.URL{})
+		p3.Transport = tr3
 		logger := logrus.New()
 		logger.SetOutput(io.Discard)
-		rb := rebalancer.New([]*httputil.ReverseProxy{p1, p2}, mockLb, logger)
-		go rb.StartRebalancer()
-		time.Sleep(2 * time.Second)
+		rb := rebalancer.New([]*httputil.ReverseProxy{p1, p2, p3}, mockLb, logger)
+		go rb.StartRebalancer(time.Millisecond*200, "https://foo\\x7f.com/")
+		time.Sleep(time.Microsecond * 250)
+	})
+	t.Run("it returns the same load balancer that was provided", func(t *testing.T) {
+		lb := roundrobin.New[*httputil.ReverseProxy](nil)
+		rb := rebalancer.New(nil, lb, nil)
+		assert.Same(t, lb, rb.GetLoadbalancer())
 	})
 }
